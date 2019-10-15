33ea742b73989b3d6ddee343ed51f380bae770eb
nipunaupeksha <nipuna.upeksha@gmail.com>
2019-10-15 07:22:47 +0530

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,3 +1,7 @@
+SNAKE_SPEED = 100
+local snakeX, snakeY = 0, 0
+local snakeMoving = 'right'
+
 function love.load()
     love.window.setTitle('snake50')
 end
@@ -6,12 +10,23 @@ function love.keypressed(key)
     if key=='escape' then
         love.event.quit()
     end
+
+    if kry == 'left' then
+        snakeMoving = 'left'
+    else if key == 'right' then 
+        snakeMoving = 'right'
+    end
 end
 
 function love.update(dt)
-
+    if snakeMoving == 'left' then
+        snakeX = snakeX - SNAKE_SPEED * dt;
+    else 
+        snakeX = snakeX + SNAKE_SPEED * dt;
+    end
 end
 
 function love.draw()
-    love.graphics.rectangle('fill', 0, 0, 16, 16)
+    love.graphics.setColor(0,1,0,1)
+    love.graphics.rectangle('fill', snakeX, 0, 16, 16)
 end
