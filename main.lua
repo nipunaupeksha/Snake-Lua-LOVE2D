315e804f78d44e143198e31bbce46d6b6ab17a4b
nipunaupeksha <nipuna.upeksha@gmail.com>
2019-10-15 09:17:41 +0530

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,9 +1,20 @@
 SNAKE_SPEED = 100
+
+TILE_SIZE = 32
+WINDOW_WIDTH = 1280
+WINDOW_HEIGHT =  720
+
+MAX_TILES_X = WINDOW_WIDTH / TILE_SIZE
+MAX_TILES_Y = math.floor(WINDOW_HEIGHT / TILE_SIZE) + 1
+local tileGrid = {}
 local snakeX, snakeY = 0, 0
 local snakeMoving = 'right'
 
 function love.load()
     love.window.setTitle('snake50')
+    love.window.setMode(WINDOW_WIDTH, WINDOW_HEIGHT, {
+        fullscreen = false
+    })
 end
 
 function love.keypressed(key)
@@ -35,6 +46,21 @@ function love.update(dt)
 end
 
 function love.draw()
-    love.graphics.setColor(0,1,0,1)
-    love.graphics.rectangle('fill', snakeX, snakeY, 16, 16)
+    drawGrid()
+    drawSnake()
 end
+
+function drawGrid()
+    love.graphics.setColor(0,1,1,1)
+    for y = 1,MAX_TILES_Y do
+        for x = 1, MAX_TILES_X do
+            love.graphics.rectangle('line',(x-1)*TILE_SIZE,(y-1)*TILE_SIZE,TILE_SIZE,TILE_SIZE)
+        end
+    end
+end
+
+function drawSnake()
+    love.graphics.setColor(0,1,0,1)
+    love.graphics.rectangle('fill', snakeX, snakeY, TILE_SIZE, TILE_SIZE)
+end
+
